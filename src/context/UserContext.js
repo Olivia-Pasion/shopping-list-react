835a1e21a15ecdc20ea12fbbbf3e9592b10c1910
olivia pasion <oap9820@gmail.com>
2022-09-28 12:03:12 -0700

    building out UserContext.js

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,1 +1,14 @@
-// TODO -- create the user provider!
+import { createContext, useContext, useState } from 'react';
+
+const UserContext = createContext();
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(initialUser);
+
+  return (
+    <UserContext.Provider value={{user, setUser}}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+const useUserContext = 
