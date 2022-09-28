c68c216da65a31475cf3062e1cc2a68728c44d41
olivia pasion <oap9820@gmail.com>
2022-09-28 12:13:53 -0700

    UserContext.js done

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,14 +1,24 @@
 import { createContext, useContext, useState } from 'react';
+import { getUser } from '../services/auth';
 
 const UserContext = createContext();
 const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(initialUser);
+  const [user, setUser] = useState(getUser);
 
   return (
-    <UserContext.Provider value={{user, setUser}}>
+    <UserContext.Provider value={{ user, setUser }}>
       {children}
     </UserContext.Provider>
   );
 };
 
-const useUserContext = 
+const useUserContext = () => {
+  const context = useContext(UserContext);
+
+  if (context === undefined) {
+    throw new Error('useUserContext must be used within a UserProvider');
+  }
+  return context;
+};
+
+export { UserProvider, useUserContext };
